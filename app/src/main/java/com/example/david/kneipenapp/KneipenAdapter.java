0426df98414640c8416580917fb0d6138c35734a
Nicholas Ebi <nicholas.ebi@hs-furtwangen.de>
2015-05-07 15:27:27 +0200

    DO WERK!

--- app/src/main/java/com/example/david/kneipenapp/KneipenAdapter.java
+++ app/src/main/java/com/example/david/kneipenapp/KneipenAdapter.java
@@ -1,6 +1,8 @@
 package com.example.david.kneipenapp;
 
 import android.app.LauncherActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 public class KneipenAdapter extends RecyclerView.Adapter<KneipenAdapter.ListItemViewHolder> {
 
     private final ArrayList<Lokal> list;
+    private static Context sContext;
 
     public static class ListItemViewHolder extends RecyclerView.ViewHolder {
 
@@ -22,6 +25,7 @@ public class KneipenAdapter extends RecyclerView.Adapter<KneipenAdapter.ListItem
         protected TextView typ;
         protected TextView stars;
         protected ImageView image;
+        protected int src;
 
         public ListItemViewHolder(View itemView) {
             super(itemView);
@@ -31,11 +35,20 @@ public class KneipenAdapter extends RecyclerView.Adapter<KneipenAdapter.ListItem
             typ = (TextView) itemView.findViewById(R.id.type);
             stars = (TextView) itemView.findViewById(R.id.rating);
             image = (ImageView) itemView.findViewById(R.id.imageView);
+            src = R.id.imageView;
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Toast.makeText(view.getContext(),headlineView.getText(),Toast.LENGTH_LONG).show();
+                    final Intent intent = new Intent(sContext, DetailActivity.class);
+                    intent.putExtra("headline", headlineView.getText());
+                    intent.putExtra("address", addressView.getText());
+                    intent.putExtra("typ", typ.getText());
+                    intent.putExtra("stars", stars.getText());
+                    intent.putExtra("src", src);
+
+                    sContext.startActivity(intent);
                 }
             });
         }
@@ -48,6 +61,7 @@ public class KneipenAdapter extends RecyclerView.Adapter<KneipenAdapter.ListItem
     @Override
     public ListItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_view_layout, viewGroup, false);
+        sContext = viewGroup.getContext();
         return new ListItemViewHolder(itemView);
     }
 
